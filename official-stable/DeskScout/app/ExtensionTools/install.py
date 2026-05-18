d4c5c76bf393787b8010758c98d0eef5e202101e
Github73840134 <35036025+Github73840134@users.noreply.github.com>
2026-05-18 17:57:12 -0400

    Build 44

--- official-stable/DeskScout/app/ExtensionTools/install.py
+++ official-stable/DeskScout/app/ExtensionTools/install.py
@@ -12,7 +12,7 @@ from win32more.Windows.UI.Xaml.Interop import TypeName
 from win32more.Windows.UI.Xaml import UIElement
 from win32more.Windows.Win32.System.Threading import Sleep
 from win32more import Windows
-from win32more.xaml import XamlApplication
+from win32more.winui3 import XamlApplication
 from win32more.Microsoft.UI.Xaml import Window, FrameworkElement
 from win32more.Microsoft.UI.Xaml.Media import MicaBackdrop,Imaging,FontFamily,CompositionTarget,VisualTreeHelper
 from win32more.Microsoft.UI.Xaml.Markup import XamlReader
